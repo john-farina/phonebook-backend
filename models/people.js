a658ba6ced8665b53cc4774674e8b29fe7226a96
onefootfarina <clichecontent@gmail.com>
2022-07-15 21:04:21 -0400

    wow my password

--- models/people.js
+++ models/people.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI;
+const url =
+    'mongodb+srv://fullstack:firstapp@cluster0.zi5k6om.mongodb.net/?retryWrites=true&w=majority';
 
 console.log('connecting to ', url);
 mongoose
